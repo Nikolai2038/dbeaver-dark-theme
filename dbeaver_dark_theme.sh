525fcee15b14a4a572b99e91ce71cf2c35d20ff5
Nikolai2038 <nikolai2038@yandex.ru>
2025-03-08 17:44:45 +0300

    More info in logs

--- dbeaver_dark_theme.sh
+++ dbeaver_dark_theme.sh
@@ -18,25 +18,26 @@ main() {
 
   [ "$#" -gt 0 ] && { action="${1}" && shift || return "$?"; } || action=""
   if [ "${action}" = "install" ]; then
-    echo "Creating backup \"${css_dir_backup}\"..." >&2
+    echo "Creating backup from \"${css_dir_default}\" to \"${css_dir_backup}\"..." >&2
     if [ -d "${css_dir_backup}" ]; then
-      echo "Creating backup \"${css_dir_backup}\": skipped! Backup already exists!" >&2
+      echo "Creating backup from \"${css_dir_default}\" to \"${css_dir_backup}\": skipped! Backup already exists!" >&2
     else
       cp -rT "${css_dir_default}" "${css_dir_backup}" || return "$?"
-      echo "Creating backup \"${css_dir_backup}\": success!" >&2
+      echo "Creating backup from \"${css_dir_default}\" to \"${css_dir_backup}\": success!" >&2
     fi
 
-    echo "Applying CSS files from \"${css_files_dir}\"..." >&2
+    echo "Applying CSS files from \"${css_files_dir}\" to \"${css_dir_default}\"..." >&2
+    sudo rm -rf "${css_dir_default}" || return "$?"
     sudo cp -rT "${css_files_dir}" "${css_dir_default}" || return "$?"
-    echo "Applying CSS files from \"${css_files_dir}\": success!" >&2
+    echo "Applying CSS files from \"${css_files_dir}\" to \"${css_dir_default}\": success!" >&2
   elif [ "${action}" = "uninstall" ]; then
-    echo "Applying backup \"${css_dir_backup}\"..." >&2
+    echo "Applying backup from \"${css_dir_backup}\" to \"${css_dir_default}\"..." >&2
     if [ ! -d "${css_dir_backup}" ]; then
       echo "Backup \"${css_dir_backup}\" not found! Maybe \"dbeaver_dark_theme\" was already uninstalled." >&2
       return 1
     fi
     sudo cp -rT "${css_dir_backup}" "${css_dir_default}" || return "$?"
-    echo "Applying backup \"${css_dir_backup}\": success!" >&2
+    echo "Applying backup from \"${css_dir_backup}\" to \"${css_dir_default}\": success!" >&2
 
     rm -rf "${css_dir_backup}" || return "$?"
     echo "Backup \"${css_dir_backup}\" removed!" >&2
